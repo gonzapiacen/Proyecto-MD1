55b2b898b1812cb5cf0316540e97b59cf9cd32ae
PcDamianz1 <cristianpaz423@gmail.com>
2026-06-01 23:56:58 -0300

    Intento de mejora de la Ia, pero fallo, no modificacion grandes.

---
+++ Assets/Scripts/Enemy/ENEMY_TEST_CRIS.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ENEMY_TEST_CRIS: MonoBehaviour
+{
+    //IGUAL QUE OTRO CODIGO PERO PARA EVITAR ERRORES HICE ESTE.
+    //AGREGA MOVIEMIENTO y PERSECUCION.
+
+    NavMeshAgent Agente;
+    [SerializeField] Transform Target;
+    private Animator anim;
+    private bool TrueTarget;
+    [SerializeField] private float RangAt = 2f;
+    [SerializeField] private float SpeedEnemy = 1f;
+    [SerializeField] private float AccelarationEnemy = 4f;
+
+    [SerializeField] float RangodeVision = 10;
+
+    private bool stunned = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Agente = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        ControlEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!stunned)
+        {
+            if (Target != null)
+            {
+                Agente.SetDestination(Target.position);
+                TrueTarget = true;
+            }
+            else
+            {
+                TrueTarget = false;
+                if (Agente.hasPath)
+                {
+                    Agente.ResetPath();
+                }
+            }
+            AttackEnemy();
+        }
+        Animation();
+    }
+
+    private void VisionEnte()
+    {
+       
+    }
+
+    public void Animation()
+    {
+        if (TrueTarget != false && !stunned)
+        {
+            anim.SetBool("Target_lock", true);
+        }
+        else
+        {
+            anim.SetBool("Target_lock", false);
+        }
+    }
+
+    public void AttackEnemy()
+    {
+        if (Target != null)
+        {
+            float _distancia = Vector3.Distance(transform.position, Target.position);
+
+            if (_distancia <= RangAt)
+            {
+                anim.SetBool("Target_Attack", true);
+            }
+            else
+            {
+                anim.SetBool("Target_Attack", false);
+            }
+
+        }
+
+    }
+    
+    public void ControlEnemy()
+    {
+        Agente.speed = SpeedEnemy;
+        Agente.acceleration = AccelarationEnemy;
+    }
+
+    public void GetStun()
+    {
+        stunned = true;
+        Agente.isStopped = true;
+        Debug.Log("Enemy Stun");
+        Invoke("UnStun", 7f);
+    }
+
+    private void UnStun()
+    {
+        stunned = false;
+        Agente.isStopped = false;
+        Debug.Log("Enemy Unstun");
+    }
+
+}
